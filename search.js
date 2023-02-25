03a7affdfb7e30f655df635e8ccfdcdaa7ebd8b1
abdoufuntuber@gmail.com <abdoufuntuber@gmail.com>
2023-02-25 20:05:09 +0100

    added search functionality and css for the search result container

--- search.js
+++ search.js
@@ -1,11 +1,27 @@
 import React, { useState } from "react";
+import products from "./productsObj";
+//import "./style.css";
 
 const SearchIcon = () => {
   const [isExpanded, setIsExpanded] = useState(false);
+  const [searchResults, setSearchResults] = useState([]);
 
   const handleSearchClick = () => {
     setIsExpanded(!isExpanded);
-  }
+  };
+
+  const handleSearch = (e) => {
+    const searchValue = e.target.value.toLowerCase();
+    const results = Object.keys(products)
+      .map(category =>
+        products[category].filter(product =>
+          product.name.toLowerCase().includes(searchValue)
+        )
+      )
+      .reduce((acc, val) => acc.concat(val), []);
+
+    setSearchResults(results);
+  };
 
   return (
     <div className="search-container">
@@ -13,11 +29,21 @@ const SearchIcon = () => {
         <i className="fa fa-search"></i>
       </button>
       <div className={`search-input-container ${isExpanded ? 'expanded' : ''}`}>
-        <input type="text" placeholder="Search..." />
+        <input type="text" placeholder="Search..." onChange={handleSearch} />
+        {searchResults.length > 0 && (
+          <div className="search-results-container">
+            {searchResults.map((result) => (
+              <div key={result.id}>
+                <img src={result.image} alt={result.name} />
+                <p>{result.name}</p>
+                <p>{result.price}</p>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
-}
-
+};
 
 export default SearchIcon;
